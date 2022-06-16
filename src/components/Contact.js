9ed73a406567f61cc2587173b4489dbf57352098
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-06-16 00:47:08 -0500

    Complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,13 @@ export default function Contact() {
          </div>
 
          <div className='flex justify-center items-center'>
-            <form action="" className='flex flex-col w-full md:w-1/2 mx-auto'>  
+            <form action="https://getform.io/f/415db701-8f8d-4846-8fad-9847f90b3552" method='POST' className='flex flex-col w-full md:w-1/2 mx-auto'>  
                <input type="text" name="name" placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                <input type="email" name="email" placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
                <textarea name="message" placeholder='Enter your message' rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline:none'>
                </textarea>
 
-               <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items bg-center rounded-md hover:scale-110'></button>
+               <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items bg-center rounded-md hover:scale-110'>Submit</button>
             
             </form>
          </div>
