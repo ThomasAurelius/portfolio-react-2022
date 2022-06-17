db892dd1112d5e06cc87576e542af3ac28358fe9
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-06-16 21:46:52 -0500

    size adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
     <div>   
       <div class="footer bg-gradient-to-b from-gray-800 to-black text-white  lg:px-12">
 
-				<h3 className='font-bold text-2xl'>Contact me!</h3>
-				<a href="mailto:thomas.aurelius.widmer@gmail.com" className="footer_link text-2xl">thomas.aurelius.widmer@gmail.com</a>
+				<h3 className='font-bold'>Contact me!</h3>
+				<a href="mailto:thomas.aurelius.widmer@gmail.com" className="footer_link  md:text-2xl ">thomas.aurelius.widmer@gmail.com</a>
 				<ul className="social-list my-4">
 					<li className="social-list_item">
 						<a className="social-list_link" href="https://www.codewars.com/users/ThomasAurelius" target="_blank" rel="noreferrer">
