df341fa38afd9eb7b7ef02c52f7c29b7887f2435
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-06-17 00:40:36 -0500

    remove p

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -60,7 +60,7 @@ export default function Portfolio() {
       <div className='max-w-screen-lg p-4 pt-10 mx-auto flex flex-col justify-center w-full h-full'>
          <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>Portfolio</p>
-            <p className='py-6'>Check out some of my latest work:</p>
+            
          </div>
          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 m-1' >
 
