b661364e09293973e97827410e5f1ec8e7461483
Thomas Aurelius <teg824@hotmail.com>
2022-06-17 21:50:03 -0500

    about

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div name="about" className='w-full h-full bg-gradient-to-b to-gray-800 from-black text-white pb-20 lg:px-12'>   
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
          <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4'>About</p>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
          </div>
          <p className='text-xl mt-10'>Ambitious and driven Software Developer focusing on Javascript and React, with a keen interest in Machine Learning and AI. I
           have a strong leadership background and enjoy mentoring other developers, and supporting the dev community. I enjoy the creative process of software development and overcoming new challenges. As a process and personal improvement evangelist, I have a deep belief in the power of leaving everything better than I found it.
