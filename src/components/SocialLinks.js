cb9820ddbc2374d5725ad11526f30d6de588e3bf
Thomas Aurelius <teg824@hotmail.com>
2022-07-01 22:08:56 -0500

    resume

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -55,7 +55,7 @@ const links = [
          <> Resume <BsFillPersonLinesFill size={30} />  
          </>
       ),
-      href: '/thomas_gooch_resume_061722.pdf',
+      href: '/thomas_gooch_resume_070122.pdf',
       style: 'rounded-br-md',
       download: true,
    }
